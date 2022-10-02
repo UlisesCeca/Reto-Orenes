665ca4909e3805fc4e80305159d503bda700e866
DESKTOP-B9K728K\Ulises <ulises@ulisesceca.com>
2022-10-02 13:55:12 +0200

    added tests for the retrieval of a vehicle location

--- src/test/java/com/orenes/reto/services/LocationServiceTests.java
+++ src/test/java/com/orenes/reto/services/LocationServiceTests.java
@@ -59,5 +59,48 @@ public class LocationServiceTests {
 	    });
 		assertEquals(exception.getMessage(), EXPECTED_NOT_FOUND_MESSAGE + VEHICLE_PLATE_NUMBER);
 	}
+	
+	/**
+	 * Test that checks if the retrieval of a vehicle's location is correct and it exists.
+	 */
+	@Test
+	public void getVehicleLocation_0K01() {
+		final String VEHICLE_PLATE_NUMBER = "111111A";
+		final Location initialLocation = new Location(1234L, 4321L);
+		final Location updatedLocation; 
+		VehicleDAO vehicleDAO = this.vehicleRepository.findByPlateNumber(VEHICLE_PLATE_NUMBER).get();
+		
+		assertThat(vehicleDAO.getLastLocation()).isNull();
+		this.locationService.updateVehicleLocation(initialLocation, vehicleDAO.getPlateNumber());
+		updatedLocation = this.locationService.getVehicleLocation(VEHICLE_PLATE_NUMBER);
+		assertThat(updatedLocation).isNotNull();
+		assertEquals(initialLocation.getLatitude(), updatedLocation.getLatitude());
+		assertEquals(initialLocation.getLongitude(), updatedLocation.getLongitude());
+	}
+	
+	/**
+	 * Test that checks if the retrieval of a vehicle's location is correct but it doesn't exist.
+	 */
+	@Test
+	public void getVehicleLocation_0K02() {
+		final String VEHICLE_PLATE_NUMBER = "111111A";
+		final Location updatedLocation; 
+		updatedLocation = this.locationService.getVehicleLocation(VEHICLE_PLATE_NUMBER);
+		assertThat(updatedLocation.getLatitude()).isNull();
+		assertThat(updatedLocation.getLongitude()).isNull();
+	}
+	
+	/**
+	 * Test that checks if an exception is thrown when the specified plate number of a car
+	 * doesn't exist.
+	 */
+	@Test
+	public void getVehicleLocation_KO01() {
+		final String VEHICLE_PLATE_NUMBER = "33333B";
+		final Exception exception = assertThrows(VehicleNotFoundException.class, () -> {
+			this.locationService.getVehicleLocation(VEHICLE_PLATE_NUMBER);
+	    });
+		assertEquals(exception.getMessage(), EXPECTED_NOT_FOUND_MESSAGE + VEHICLE_PLATE_NUMBER);
+	}
 
 }
