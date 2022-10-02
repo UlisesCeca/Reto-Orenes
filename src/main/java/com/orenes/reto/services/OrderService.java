2bcd4286fd0c2c3cf3ffcde7c92cd10d750b3d5e
DESKTOP-B9K728K\Ulises <ulises@ulisesceca.com>
2022-10-02 15:29:58 +0200

    finished the service to insert new orders

--- src/main/java/com/orenes/reto/services/OrderService.java
+++ src/main/java/com/orenes/reto/services/OrderService.java
@@ -2,10 +2,11 @@ package com.orenes.reto.services;
 
 import org.springframework.stereotype.Service;
 
+import com.orenes.reto.exceptions.OrderIDAlreadyExistsException;
 import com.orenes.reto.services.entities.Order;
 
 /**
- * Interface that defines the bussiness logic regarding the location of the vehicles
+ * Interface that defines the bussiness logic regarding the orders of the clients.
  * 
  * @author Ulises Ceca
  * @version 1.0
@@ -16,7 +17,8 @@ public interface OrderService {
 	 * Inserts a new order into the database.
 	 *
 	 * @param newOrder the new location order to be inserted
+	 * @throws OrderIDAlreadyExistsException if the order ID already exists
 	 * @return the inserted order
 	 */
-	Order insertOrder(final Order newOrder);
+	Order insertOrder(final Order newOrder) throws OrderIDAlreadyExistsException;
 }
--- src/main/java/com/orenes/reto/services/impl/OrderServiceImpl.java
+++ src/main/java/com/orenes/reto/services/impl/OrderServiceImpl.java
@@ -1,8 +1,11 @@
 package com.orenes.reto.services.impl;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
+import com.orenes.reto.exceptions.OrderIDAlreadyExistsException;
 import com.orenes.reto.repositories.OrderRepository;
+import com.orenes.reto.repositories.dao.OrderDAO;
 import com.orenes.reto.services.OrderService;
 import com.orenes.reto.services.entities.Order;
 
@@ -15,15 +18,32 @@ import com.orenes.reto.services.entities.Order;
 @Service
 public class OrderServiceImpl implements OrderService{
 	private final OrderRepository orderRepository;
+	private final ModelMapper modelMapper;
 	
-	public OrderServiceImpl(final OrderRepository orderRepository) {
+	public OrderServiceImpl(final OrderRepository orderRepository, final ModelMapper modelMapper) {
 		this.orderRepository = orderRepository;
+		this.modelMapper = modelMapper;
 	}
 
+	/**
+	 * Inserts a new order into the database.
+	 *
+	 * @param newOrder the new location order to be inserted
+	 * @throws OrderIDAlreadyExistsException if the order ID already exists
+	 * @return the inserted order
+	 */
 	@Override
-	public Order insertOrder(final Order newOrder) {
-		// TODO Auto-generated method stub
-		return null;
+	public Order insertOrder(final Order newOrder) throws OrderIDAlreadyExistsException{
+		final OrderDAO newOrderDao;
+		
+		if (this.orderRepository.existsByOrderId(newOrder.getOrderId())) {
+			throw new OrderIDAlreadyExistsException(newOrder.getOrderId());
+		} else {
+			newOrderDao = this.modelMapper.map(newOrder, OrderDAO.class);
+			this.orderRepository.save(newOrderDao);
+		}
+		
+		return this.modelMapper.map(newOrderDao, Order.class);
 	}
 
 }
